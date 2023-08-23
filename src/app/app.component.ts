15854759de9e8107a8fd7f3b1fb8543d3c034902
sy0tom <sy01e90+it.git@gmail.com>
2023-08-23 20:44:18 +0900

    add: lesson_01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>Hello World!</h1>',
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
